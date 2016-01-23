d427ab5f91bcdb4527183d9b620c28aa3a1fe529
ncadiente <ncadiente@gmail.com>
2016-01-23 11:55:53 -1000

    adds toefu assets, and auto asset load

--- public/js/states/boot.js
+++ public/js/states/boot.js
@@ -7,6 +7,20 @@ ToeFu.Boot = function(){
 
 ToeFu.Boot.prototype.preLoad = function(){
   //will preload all assets
+
+  //auto load each asset by type
+  Object.keys( ToeFu.ASSETS ).forEach(
+    function(type){
+      for(var asset in ToeFu.ASSETS[type]){
+        ToeFu.game.load[ type.toLowerCase()](
+          ToeFu.ASSETS[type][asset].name,
+          ToeFu.ASSETS[type][asset].path,
+          ToeFu.ASSETS[type][asset].width,
+          ToeFu.ASSETS[type][asset].height,
+          ToeFu.ASSETS[type][asset].frames
+        );
+      }
+  });
 };
 
 ToeFu.Boot.prototype.create = function (){
